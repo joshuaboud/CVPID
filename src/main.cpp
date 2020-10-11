9d439b2ecd722b1877e495d564527d38e6141406
Josh Boudreau <joshuaboud@hotmail.com>
2020-10-11 15:38:39 -0300

    added sliders for configuring mask

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@
 const char *colour_view = "CVPID Vision";
 const char *binary_view = "Threshold Mask";
 
-cv::Point find_x_y(const cv::Mat &input, int hue_center, cv::Mat &binary, double &area){
+cv::Point find_x_y(const cv::Mat &input, int hue_center, int min_S, int min_V, cv::Mat &binary, double &area){
 	cv::Mat frame_HSV;
 	cv::cvtColor(input, frame_HSV, cv::COLOR_BGR2HSV);
-	cv::Scalar low_HSV((hue_center - 10)%180,100,100);
-	cv::Scalar high_HSV((hue_center + 10)%180,255,255);
+	cv::Scalar low_HSV((hue_center - 10)%180, min_S, min_V);
+	cv::Scalar high_HSV((hue_center + 10)%180, 255, 255);
 	cv::inRange(frame_HSV, low_HSV, high_HSV, binary);
 	//cv::ThresholdTypes type = (invert)? cv::THRESH_BINARY_INV : cv::THRESH_BINARY;
 	//cv::threshold(grey, binary, threshold, 255, type);
@@ -33,8 +33,6 @@ void draw_box(cv::Mat &img, cv::Point loc, double circle_area){
 int main(int argc, char *argv[]){
 	double circle_area;
 	char keypress;
-	int hue_center = 30;
-	bool invert_threshold = false;
 	cv::VideoCapture camera;
 	cv::Mat image, binary;
 	
@@ -42,17 +40,26 @@ int main(int argc, char *argv[]){
 	cv::namedWindow(colour_view, 1);
 	// Create a window
 	cv::namedWindow(binary_view, 1);
+	const int hue_slider_max = 179;
+	const int SV_slider_max = 254;
+	int hue_center = 30;
+	int min_S = 0;
+	int min_V = 0;
+	cv::createTrackbar("Hue", binary_view, &hue_center, hue_slider_max, NULL);
+	cv::createTrackbar("Min Saturation", binary_view, &min_S, SV_slider_max, NULL);
+	cv::createTrackbar("Min Value", binary_view, &min_V, SV_slider_max, NULL);
 	
 	// open camera 0
 	if(!camera.open(0)){
 		printf("Error opening camera 0");
 		return -1;
 	}
-	
+	camera.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	camera.set(cv::CAP_PROP_FRAME_HEIGHT, 360);
 	while(true){
 		camera >> image;
 		
-		cv::Point centroid = find_x_y(image, hue_center, binary, circle_area);
+		cv::Point centroid = find_x_y(image, hue_center, min_S, min_V, binary, circle_area);
 		
 		draw_box(image, centroid, circle_area);
 		
@@ -62,10 +69,6 @@ int main(int argc, char *argv[]){
 		keypress = (char)cv::waitKey(1);
 		if(keypress == (char)27)
 			break;
-		if(keypress == '+')
-			hue_center += 5;
-		if(keypress == '-')
-			hue_center -= 5;
 	}
 	
 	return 0;
