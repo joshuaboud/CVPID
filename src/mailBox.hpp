3d6f75b09c51d9b9dc95101c45e9aee6a3b6f3c3
Josh Boudreau <joshuaboud@hotmail.com>
2020-10-15 13:15:52 -0300

    created mailbox class for passing object between threads

---
+++ src/mailBox.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+template <class T>
+class MailBox{
+private:
+	std::counting_semaphore sem;
+	std::mutex full;
+	T package;
+public:
+	friend cv::VideoCapture &operator>>(
+		cv::VideoCapture &in,
+		MailBox<cv::Mat> &mb
+	){
+		mb.full.lock();
+		in >> mb.package;
+		mb.sem.release();
+		return in;
+	}
+	void put(T &in){
+		full.lock();
+		package = T(in);
+		sem.release();
+	}
+	T get(void){
+		sem.acquire();
+		T out = T(package);
+		full.unlock();
+		return out;
+	}
+};
