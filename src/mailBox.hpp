39e1b01c91c3ad0c78fefb728e60fe4355bd4aa4
Josh Boudreau <joshuaboud@hotmail.com>
2020-10-15 17:58:11 -0300

    add copyright disclaimers

--- src/mailBox.hpp
+++ src/mailBox.hpp
@@ -1,7 +1,26 @@
+/* Copyright (C) 2020 Joshua Boudreau, Micah MacLean
+ * 
+ * This file is part of CVPID.
+ * 
+ * CVPID is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CVPID is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CVPID.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <mutex>
 #include <opencv2/opencv.hpp>
+
 //#include <semaphore> C++20
 #include "semaphore.hpp"
 
--- src/semaphore.hpp
+++ src/semaphore.hpp
@@ -1,3 +1,21 @@
+/* Copyright (C) 2020 Joshua Boudreau, Micah MacLean
+ * 
+ * This file is part of CVPID.
+ * 
+ * CVPID is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CVPID is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CVPID.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <mutex>
 #include <condition_variable>
         
