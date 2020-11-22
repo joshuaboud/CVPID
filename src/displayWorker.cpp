222523839a4d6d470814072490ccce6939732870
Josh Boudreau <joshuaboud@hotmail.com>
2020-11-22 18:41:14 -0400

    update defaults

--- src/displayWorker.cpp
+++ src/displayWorker.cpp
@@ -90,8 +90,8 @@ void display(MailBox<BlobInfo> &display_in, ProcParams &params, PID &x_control, 
 	const int SV_slider_max = 254;
 	const int min_A_slider_max = 1000;
 	params.hue_center = 105;
-	params.min_S = 61;
-	params.min_V = 14;
+	params.min_S = 76;
+	params.min_V = 5;
 	params.min_A = 107;
 	cv::createTrackbar("Hue", binary_view, &params.hue_center, hue_slider_max, NULL);
 	cv::createTrackbar("Min Saturation", binary_view, &params.min_S, SV_slider_max, NULL);
